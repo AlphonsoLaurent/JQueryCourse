84f34e79b64cb4ca60bb6595306408e3e5735908
piolo94 <ang_maya94@hotmail.com>
2019-08-13 22:56:52 -0500

    Primera clase JavaScript Martes 13 de agosto 2019 Angel Maya.

---
+++ javascript/ejemplo.js
@@ -0,0 +1,28 @@
+var numero = 1;
+var suma = numero + 2;
+var miArray = [1,4,9,16,25];
+var miArrayMixed = [1,'Jack',5,true,'Pedro',3.5];
+var miVariable;
+
+console.error(suma);
+console.log(miArray[3]);
+console.log(miArrayMixed[5]);
+
+var auto={
+	numLlantas:4,
+	numPuertas:2,
+	numAcientos:4,
+	numEspejos:2,
+	numPlacas:'4jfyz',
+	color:'red',
+	modelo:'mini',
+	litrosGas:18.7,
+
+	parabrisas:{
+		ancho:12,
+		alto:1.5,
+		material:'vidrio'
+	}
+};
+
+console.log(auto);
